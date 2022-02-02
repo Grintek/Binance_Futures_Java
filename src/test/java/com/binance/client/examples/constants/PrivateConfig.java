d72ff37fb4c2fee3f1b0ef93f9a109b035446c73
Grisha <grisha2@bk.ru>
2022-02-02 18:01:35 +0300

    commit

--- src/test/java/com/binance/client/examples/constants/PrivateConfig.java
+++ src/test/java/com/binance/client/examples/constants/PrivateConfig.java
@@ -2,7 +2,7 @@ package com.binance.client.examples.constants;
 
 public class PrivateConfig {
 
-    public static final String API_KEY = "XXXXXX";
-    public static final String SECRET_KEY = "XXXXXX";
+    public static final String API_KEY = "039514d0d1b67e0c095a1963dbda03c33a74f04b19c8e7ce9d9be3a326c537a3";
+    public static final String SECRET_KEY = "03a8ba604966520956c36711463b0c64dfda4c29d183cb72bdae7672332de648";
 
 }
--- src/test/java/com/binance/client/examples/trade/PostOrder.java
+++ src/test/java/com/binance/client/examples/trade/PostOrder.java
@@ -9,6 +9,7 @@ import com.binance.client.model.enums.*;
 public class PostOrder {
     public static void main(String[] args) {
         RequestOptions options = new RequestOptions();
+        options.setUrl("https://testnet.binancefuture.com");
         SyncRequestClient syncRequestClient = SyncRequestClient.create(PrivateConfig.API_KEY, PrivateConfig.SECRET_KEY,
                 options);
 //        System.out.println(syncRequestClient.postOrder("BTCUSDT", OrderSide.SELL, PositionSide.BOTH, OrderType.LIMIT, TimeInForce.GTC,
@@ -16,7 +17,7 @@ public class PostOrder {
 
         // place dual position side order.
         // Switch between dual or both position side, call: com.binance.client.examples.trade.ChangePositionSide
-        System.out.println(syncRequestClient.postOrder("BTCUSDT", OrderSide.SELL, PositionSide.SHORT, OrderType.LIMIT, TimeInForce.GTC,
-                "1", "9000", null, null, null, null, NewOrderRespType.RESULT));
+        System.out.println(syncRequestClient.postOrder("BTCUSDT", OrderSide.BUY, null, OrderType.MARKET, null,
+                "1", null, null, null, null, null, NewOrderRespType.RESULT));
     }
 }
